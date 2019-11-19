586fe2721ddcf057d8d1b79d8939684cd5a8aefa
Amy Braswell <braswellamy@mac.com>
2019-11-18 22:06:39 -0500

    final version before submitting for approval

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 
 
-
-// api Auth Header:
-//curl -H 'X-Api-Key: KFzjIXeflsLaqnKagKQ3wT4KeZPhpIkD3HaFaaKn' 'https://developer.nps.gov/api/v1/parks'
-
-// RETURN
-// data.fullName
-// data.description
-// data.url
-
-
 'use strict';
 
-// right now apiKey is built into url - put in header eventually
 const apiKey = 'KFzjIXeflsLaqnKagKQ3wT4KeZPhpIkD3HaFaaKn'; 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
-
-
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -24,49 +11,57 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function getParksByState(query, maxResults=10, api_key,) {
+function getParksByState(query, limit=10, api_key) {
   const params = {
     stateCode: query,
-    maxResults,
+    limit,
     api_key: apiKey
   };
 
-  const queryString = formatQueryParams(params);
-  const url = searchURL + '?' + queryString;
-  console.log({url, params});  // self-commenting "url: url params: param"
 
-  /*
-  const options = {
-    headers: new Headers({
-      "X-Api-Key": apiKey})
-  };
-  */
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString;
   
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => console.log(JSON.stringify(responseJson)))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+            return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
 }
 
+
+function displayResults(responseJson) {
+    // if there are previous results, remove them
+    $('#results-list').empty();
+
+    //iterate through the articles array, stopping at the end of the repo list
+    for (let i = 0; i < responseJson.data.length ; i++){
+      // add a list item to the results list for each repo in the response array,
+      // list with the repo title and link to the repo
+        $('#results-list').append(
+          `<li>
+              <h3>${responseJson.data[i].fullName}:</h3><p>${responseJson.data[i].description}</br><a href="${responseJson.data[i].url}" target="#">${responseJson.data[i].url}</a></p>
+          </li>`
+        )};
+    
+    //display the results section  
+    $('#results').removeClass('hidden');
+};
+
+
 function watchForm() {
-    console.log("watchForm ran");
   $('form').submit(event => {
     event.preventDefault();
     const searchState = $('#js-search-state').val();
     const maxResults = $('#js-max-results').val();
     getParksByState(searchState, maxResults);
-    console.log({searchState});
-    console.log({maxResults});
   });
 }
-
-
 
 $(watchForm);
